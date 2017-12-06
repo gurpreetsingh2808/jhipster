445ff2901fc91c41175914a10982b5e4dbcdd903
Gurpreet Singhgit config --global user.email gurpreetsingh2808@gmail.com <gurpreetsingh2808@gmail.com>
2017-12-06 16:11:11 +0530

    Initial application generated by JHipster-4.11.1

---
+++ src/main/java/com/sample/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.sample.jhipster.repository;
+
+import com.sample.jhipster.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/sample/jhipster/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.sample.jhipster.service.dto;
